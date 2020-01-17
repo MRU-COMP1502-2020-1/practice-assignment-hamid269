e62884601db83cba6b8a3f99f5e4ac0b794d1c56
hamid269 <hamid269@B107-014.ad.mtroyal.ca>
2020-01-17 10:29:58 -0700

    first push V 1.0

---
+++ src/isPrime/isPrime.java
@@ -0,0 +1,42 @@
+package isPrime;
+
+public class isPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Prime!");
+
+		System.out.println(isPrime(2));
+		System.out.println(isPrime(3));
+		System.out.println(isPrime(4));
+		System.out.println(isPrime(10));
+		System.out.println(isPrime(11));
+		System.out.println(isPrime(12));
+	}
+
+	public static boolean isPrime(int number) {
+	boolean prime = true;
+		for (int i = 2; i < (number / 2) + 1; i++) {
+
+			if(isDivisible(number, i)){
+				prime=false;
+				break;
+			}
+
+		}
+
+		return prime;
+
+	}
+
+	public static boolean isDivisible(int dividend, int divisor) {
+		boolean divisible = true;
+		if (dividend % divisor == 0) {
+			divisible=true;
+			
+		} else {
+			divisible=false; 
+		}
+		return divisible; 
+	}
+}
